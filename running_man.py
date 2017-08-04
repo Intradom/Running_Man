cd3a71df36b85f8674454eefe81f13e255574932
jumeng <jumeng@umich.edu>
2017-08-04 00:22:42 -0400

    finished jumping

--- running_man.py
+++ running_man.py
@@ -9,28 +9,49 @@ BLUE = (0, 0, 255)
 
  # System parameters
 FPS = 60
-
-# Game parameters
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SPEED = 5
+PLATFORMY = SCREEN_HEIGHT - 150
+# Game parameters
+SPEED = 5
+Y_VELOCITY_START = 20
+Y_ACCELERATION = 0.5
+
+DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  
 
 def get_events():
+    jump = False
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
 
-def update_logic(x):
-    x += SPEED
-    return x
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            jump = True
 
-def update_graphics(DISPLAYSURF, x):
-    # Clear the screen
-    DISPLAYSURF.fill(WHITE)
-    
+    return jump
+
+def update_logic(x, y, jump_pressed, yVelocity):
+    if y >= PLATFORMY:
+        y = PLATFORMY
+        yVelocity = 0
+
+    if y >= PLATFORMY:
+        if jump_pressed == True:
+            yVelocity = Y_VELOCITY_START
+    else:
+        yVelocity -= Y_ACCELERATION
+        
+    x += SPEED
+    y -= yVelocity
+    print y
+    return x, y, yVelocity
+
+def update_graphics(DISPLAYSURF, x, y):
+
     pygame.draw.rect(DISPLAYSURF, BLACK, (0, SCREEN_HEIGHT - 100, SCREEN_WIDTH, 100))
-    pygame.draw.rect(DISPLAYSURF, BLUE, (x, SCREEN_HEIGHT - 150, 50, 50))
+    pygame.draw.rect(DISPLAYSURF, BLUE, (x, y, 50, 50))
 
     pygame.display.update()
 
@@ -41,12 +62,15 @@ def main():
     fpsClock = pygame.time.Clock()
 
     x = 0
+    y = PLATFORMY
+    yVelocity = 0
     while True: # main game loop
-        get_events()
+        DISPLAYSURF.fill(WHITE)
+        jump = get_events()
 
-        x = update_logic(x)
+        x, y, yVelocity = update_logic(x, y, jump, yVelocity)
 
-        update_graphics(DISPLAYSURF, x)
+        update_graphics(DISPLAYSURF, x, y)
 
         fpsClock.tick(FPS)
 
