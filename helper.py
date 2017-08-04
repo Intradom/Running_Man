235c97a0c159c0a9a201870d9ce28a03de360425
Bo <bozhang10@yahoo.com>
2017-08-03 22:15:18 -0400

    updated collision calcuation for box origin to be upper left

--- helper.py
+++ helper.py
@@ -1,11 +1,11 @@
 def point_inside(p_x, p_y, b_x, b_y, b_w, b_h):
-    return ((p_x >= b_x - b_w / 2) and (p_x <= b_x + b_w / 2) and (p_y >= b_y - b_h / 2) and (p_y <= b_y + b_y / 2))
+    return ((p_x >= b_x) and (p_x <= b_x + b_w) and (p_y >= b_y) and (p_y <= b_y + b_y))
 
 def box_collision(x1, y1, w1, h1, x2, y2, w2, h2): # Remember (0, 0) is UPPER left corner
-    top1 = y1 - h1 / 2
-    bot1 = y1 + h1 / 2
-    left1 = x1 - w1 / 2
-    right1 = x1 + w1 / 2
+    top1 = y1
+    bot1 = y1 + h1
+    left1 = x1
+    right1 = x1 + w1
     
     if (point_inside(top1, left1, x2, y2, w2, h2) or
         point_inside(top1, right1, x2, y2, w2, h2) or
