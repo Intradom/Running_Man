60150da3f0818c9a41e9f075fa48a73e385de9a2
jumeng <jumeng@umich.edu>
2017-08-03 22:42:54 -0400

    started writing game

--- running_man.py
+++ running_man.py
@@ -1,16 +1,54 @@
 import pygame, sys
 from pygame.locals import *
 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+ # System parameters
+FPS = 60
+
+# Game parameters
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SPEED = 5
+
+def get_events():
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+def update_logic(x):
+    x += SPEED
+    return x
+
+def update_graphics(DISPLAYSURF, x):
+    # Clear the screen
+    DISPLAYSURF.fill(WHITE)
+    
+    pygame.draw.rect(DISPLAYSURF, BLACK, (0, SCREEN_HEIGHT - 100, SCREEN_WIDTH, 100))
+    pygame.draw.rect(DISPLAYSURF, BLUE, (x, SCREEN_HEIGHT - 150, 50, 50))
+
+    pygame.display.update()
+
 def main():
     pygame.init()
-    DISPLAYSURF = pygame.display.set_mode((400, 300))
-    pygame.display.set_caption('Hello World!')
+    DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  
+    pygame.display.set_caption('Running Man!')
+    fpsClock = pygame.time.Clock()
+
+    x = 0
     while True: # main game loop
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-        pygame.display.update()
+        get_events()
+
+        x = update_logic(x)
+
+        update_graphics(DISPLAYSURF, x)
+
+        fpsClock.tick(FPS)
 
 if __name__ == "__main__":
     main()
